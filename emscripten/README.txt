6498f4009b3d9854c37e78b4cb6206f3782da27e
wothke <info@wothke.ch>
2014-12-27 12:49:39 +0100

    first commit

---
+++ emscripten/README.txt
@@ -0,0 +1,49 @@
+AdLibido - WebAudio port of AdPlug
+========
+
+	Copyright (C) 2014 Juergen Wothke
+
+	LICENSE
+		See individual files for specific licensing information
+
+
+AdLibido is based on "adplug-2.2.1". Some unused folders have been completely removed: "adplugdb", doc", "test". The "libbinio" dependency has dierectly been included. 
+
+Everything needed for the WebAudio version is contained in this "emscripten" folder. The 
+original AdPlug code is completely unchanged, i.e. should be trivial to merge future AdPlug 
+fixes or updates.
+
+
+You'll need Emscripten (I used the win installer on WinXP: emsdk-1.13.0-full-32bit.exe which 
+could - at the time - be found here: http://kripken.github.io/emscripten-site/docs/getting_started/downloads.html) 
+
+I did not need to perform ANY additions or manual changes on the installation. The below 
+instructions assume that the adplug-2.2.1 project folder has been moved into the main emscripten 
+installation folder (maybe not necessary) and that a command prompt has been opened within the 
+project's "emscripten" sub-folder, and that the Emscripten environment vars have been previously 
+set (run emsdk_env.bat).
+
+
+Howto build:
+
+The Web version is built using the makeEmscripten.bat that can be found in this folder. The 
+script will compile directly into the "emscripten/htdocs" example web folder, were it will create 
+the web_adplug.js library. The content of the "htdocs" can be tested by first copying into some 
+document folder of a web server (this running example shows how the code is used). 
+
+
+Background information:
+
+Because the original implementation was not touched, the code doesn't cope with the async file 
+loading approach normally taken by web apps. (You might want to have a look at my WebUADE for an 
+example of a lib that has been patched to deal with load-on-demand..) This means that whenever 
+some portion of the library tries to load something this cannot be handled as a load-on-demand 
+but the respective file must already have been loaded previously - so that it is immediately 
+available when the lib looks for it.
+
+Fortunately ApPlug makes little use of additional file loading and most of it can be dealt with
+by doing a simple pre-load, e.g. "insts.dat" (needed for KSM), "standard.bnk" (needed for ROL?),
+"adplug.db" (see "htdocs/res" folder). The only problematic case seems to be the "SCI" player 
+because it relies on "*patch.003" files that are actually specific to each music file. As a 
+workaround I'd suggest to include the respective resource files in your playlist such that they 
+will get loaded before the music file that depends on it.
---
+++ emscripten/adapter.cpp
@@ -0,0 +1,153 @@
+/*
+* This file adapts "adplug" to the interface expected by my generic JavaScript player..
+*
+* Copyright (C) 2014 Juergen Wothke
+*
+* LICENSE
+* 
+* This library is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2.1 of the License, or (at
+* your option) any later version. This library is distributed in the hope
+* that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#include "../src/adplug.h"
+#include "../src/opl.h"
+#include "../src/emuopl.h"
+#include "../src/database.h"
+
+#include "output.h"
+
+#include <stdio.h>
+#include <emscripten.h>
+
+#ifdef EMSCRIPTEN
+#define EMSCRIPTEN_KEEPALIVE __attribute__((used))
+#else
+#define EMSCRIPTEN_KEEPALIVE
+#endif
+
+#define BUF_SIZE	1024
+#define TEXT_MAX	255
+#define NUM_MAX	15
+
+short	*outputBuffer=0;
+CEmuopl	*opl= 0;
+BufPlayer	*player= 0;
+const char* infoTexts[6];
+
+char title_str[TEXT_MAX];
+char author_str[TEXT_MAX];
+char desc_str[TEXT_MAX];
+char type_str[TEXT_MAX];
+char tracks_str[NUM_MAX];
+char speed_str[NUM_MAX];
+
+CAdPlugDatabase *db= 0;
+
+struct StaticBlock {
+    StaticBlock(){
+		infoTexts[0]= title_str;
+		infoTexts[1]= author_str;
+		infoTexts[2]= desc_str;
+		infoTexts[3]= type_str;
+		infoTexts[4]= speed_str;
+		infoTexts[5]= tracks_str;
+    }
+};
+
+static StaticBlock staticBlock;
+
+
+extern "C" void emu_teardown (void)  __attribute__((noinline));
+extern "C" void EMSCRIPTEN_KEEPALIVE emu_teardown (void) {
+  if(player) { delete player; player= 0;}
+  if(opl) { delete opl; opl= 0; }
+	
+  if (outputBuffer) {free(outputBuffer); outputBuffer= 0;}
+}
+
+//extern "C" int emu_init(int sample_rate, char *basedir, char *songmodule, void *buf, unsigned long len) __attribute__((noinline));
+//extern "C" EMSCRIPTEN_KEEPALIVE int emu_init(int sample_rate, char *basedir, char *songmodule, void *buf, unsigned long len)
+extern "C" int emu_init(int sample_rate, char *basedir, char *songmodule) __attribute__((noinline));
+extern "C" EMSCRIPTEN_KEEPALIVE int emu_init(int sample_rate, char *basedir, char *songmodule)
+{
+	emu_teardown();
+	
+	outputBuffer = (short *)calloc(BUF_SIZE, sizeof(short));	
+		
+	std::string	fn = std::string(basedir) + songmodule;  
+
+	opl = new CEmuopl(sample_rate, true, true);
+	player= new BufPlayer(opl, 16, 2, sample_rate, BUF_SIZE);
+	
+	// initialize output & player
+	player->get_opl()->init();
+//	player->p = CAdPlug::factory(fn, player->get_opl(), CAdPlug::players, WebCProvider_Filesystem(buf, len));
+	player->p = CAdPlug::factory(fn, player->get_opl());
+		
+	if (db == 0) {
+		db= new CAdPlugDatabase();
+		db->load("addplug.db");
+	}
+	CAdPlug::set_database(db);
+	
+	if(!player->p) {
+		std::cout << "Error loading: " << fn << std::endl;
+		delete opl; 
+		opl= 0; 
+		return -1;
+	}	
+		
+	return 0;
+}
+
+extern "C" void emu_set_subsong(int subsong) __attribute__((noinline));
+extern "C" void EMSCRIPTEN_KEEPALIVE emu_set_subsong(int subsong)
+{
+	player->p->rewind(subsong);
+}
+
+extern "C" const char** emu_get_track_info() __attribute__((noinline));
+extern "C" const char** EMSCRIPTEN_KEEPALIVE emu_get_track_info() {
+    if (player && player->p) {
+        // more Emscripten/JavaScript friendly structure..
+		snprintf(title_str, TEXT_MAX, "%s", player->p->gettitle().c_str());
+		snprintf(author_str, TEXT_MAX, "%s", player->p->getauthor().c_str());
+		snprintf(desc_str, TEXT_MAX, "%s", player->p->getdesc().c_str());
+		snprintf(type_str, TEXT_MAX, "%s", player->p->gettype().c_str());
+		
+		snprintf(speed_str, NUM_MAX, "%d", player->p->getspeed());
+		snprintf(tracks_str, NUM_MAX, "%d",  player->p->getsubsongs());
+    }
+    return infoTexts;
+}
+
+extern "C" char* EMSCRIPTEN_KEEPALIVE emu_get_audio_buffer(void) __attribute__((noinline));
+extern "C" char* EMSCRIPTEN_KEEPALIVE emu_get_audio_buffer(void) {
+	return (char*)player->getSampleBuffer();
+}
+
+extern "C" long EMSCRIPTEN_KEEPALIVE emu_get_audio_buffer_length(void) __attribute__((noinline));
+extern "C" long EMSCRIPTEN_KEEPALIVE emu_get_audio_buffer_length(void) {
+	return player->getSampleBufferSize();
+}
+
+extern "C" int emu_compute_audio_samples() __attribute__((noinline));
+extern "C" int EMSCRIPTEN_KEEPALIVE emu_compute_audio_samples() {
+    player->frame();
+	
+	if (player->playing) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
