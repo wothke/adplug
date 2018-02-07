f55e3b7e920a4f150d01f5ab563e325ac4c91b5d
wothke <tinyrsid@wothke.ch>
2018-02-07 15:58:19 +0100

    updates for use of recent emscripten 1.37.29 (with WebAssembly support)

--- emscripten/shell-pre.js
+++ emscripten/shell-pre.js
@@ -1,4 +1,12 @@
 // create separate namespace for all the Emscripten stuff.. otherwise naming clashes may occur especially when 
 // optimizing using closure compiler..
-window.spp_backend_state_ADPLUG= {};
+window.spp_backend_state_ADPLUG= {
+	notReady: true,
+	adapterCallback: function(){}	// overwritten later	
+};
+window.spp_backend_state_ADPLUG["onRuntimeInitialized"] = function() {	// emscripten callback needed in case async init is used (e.g. for WASM)
+	this.notReady= false;
+	this.adapterCallback();
+}.bind(window.spp_backend_state_ADPLUG);
+
 var backend_AdPlug = (function(Module) {
